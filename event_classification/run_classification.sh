a4e8ac7427459a587db33dddbf696868993a9add
160610101 <781886704@qq.com>
2021-04-30 18:53:01 +0800

    add evevnt_cls sh

---
+++ event_classification/run_classification.sh
@@ -0,0 +1,26 @@
+python run_classification.py \
+--task trigger \
+--model_type bert \
+--model_name_or_path G:/预训练模型/RoBERTa/RoBERTa_zh_L12_PyTorch \
+--train \
+--do_eval \
+--do_predict \
+--evaluate_during_training \
+--data_dir ../event_classification/data \
+--do_lower_case \
+--keep_accents \
+--schema ../data/duee_event_schema.json \
+--output_dir ../event_classification/saved_dict_rb1 \
+--overwrite_output_dir \
+--max_seq_length 512 \
+--per_gpu_train_batch_size 8 \
+--per_gpu_eval_batch_size 64 \
+--gradient_accumulation_steps 1 \
+--save_steps 300 \
+--logging_steps 300 \
+--num_train_epochs 20 \
+--early_stop 3 \
+--learning_rate 3e-5 \
+--weight_decay 0.01 \
+--warmup_steps 1000 \
+--seed 1
